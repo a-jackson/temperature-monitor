d6c005da504fa8cc10908f3440bd693117d09989
Andrew Jackcson <andrew@a-jackson.co.uk>
2019-01-27 20:58:28 +0000

    Add gulp for build task

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var install = require('gulp-install');
+
+const PROD_DEST = 'dist';
+
+gulp.task('default', function() {
+    return gulp
+        .src(['./package.json'])
+        .pipe(gulp.dest(PROD_DEST))
+        .pipe(
+            install({
+                args: ['--production'],
+            }),
+        );
+});
